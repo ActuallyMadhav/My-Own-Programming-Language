e64e2f6d19fe1abc60d8bbad19b2007c3ab33df2
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-24 00:51:00 +0800

    basic is wrong need to fix but sleepy

---
+++ basic.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+
+const std::string TT_INT = "INT";
+const std::string TT_FLOAT = "FLOAT";
+const std::string TT_PLUS = "PLUS";
+const std::string TT_MINUS = "MINUS";
+const std::string TT_MUL = "MUL";
+const std::string TT_DIV = "DIV";
+const std::string TT_LPAREN = "LPAREN";
+const std::string TT_RPAREN = "RPAREN";
+
+/********************* 
+    TOKEN
+*********************/
+template <typename T>
+class Token{
+    public:
+        std::string type;
+        std::optional<T> value;
+
+        Token(std::string type, std::optional<T> value){
+            this->type = type;
+            this->value = value;
+        }
+
+        std::string repr(const Token& tk){
+            if(tk.value.has_value()){
+                return tk.type + ": " + std::to_string(*tk.value);
+            }
+            return tk.type;
+        }
+};
+
+/********************* 
+    LEXER
+*********************/
+
+class Lexer{
+    public:
+        std::string text;
+        int pos;
+        char currentChar;
+
+        Lexer(std::string text){
+            this->text = text;
+            this->pos = -1;
+            this->currentChar = NULL;
+            this->advance();
+        }
+
+        void advance(){
+            this->pos++;
+            
+            if(this->pos < this->text.size()){
+                this->currentChar = this->text[pos];
+            }
+            else{
+                this->currentChar = NULL;
+            }
+        }
+
+        std::vector<Token> make_tokens(){
+            std::vector<Token> tokens;
+            std::string whitespace = " \t";
+            
+            while(this->currentChar != NULL){
+                if(whitespace.find(this->currentChar) != std::string::npos){
+                    this->advance();
+                }
+                else if(this->currentChar == '+'){
+                    tokens.push_back(Token(TT_PLUS, std::));
+                }
+            }
+
+            return tokens;
+        }
+
+};
